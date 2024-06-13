c224eea1673daa2b908812ea63ecd096ed7cbe5f
shania524 <parweenshania@gmail.com>
2024-06-13 17:17:47 +0530

    second commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'templates', 'views'));
 
 // mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true })
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
 
 
 
